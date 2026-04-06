bcc610016f51c469116e14241088f2ce725728a2
TheRusted <benjaminthaver5599@gmail.com>
2026-04-06 09:31:33 +0800

    update 3.10

--- src/components/ProfileMidBody.jsx
+++ src/components/ProfileMidBody.jsx
@@ -1,9 +1,9 @@
-import { jwtDecode } from "jwt-decode";
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 import { Button, Col, Image, Nav, Row, Spinner } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import ProfilePostCard from "./ProfilePostCard";
 import { fetchPostsByUser } from "../features/posts/postsSlice";
+import { AuthContext } from "./AuthProvider";
 
 export default function ProfileMidBody() {
     const url =
@@ -11,18 +11,15 @@ export default function ProfileMidBody() {
     const pic =
         "https://pbs.twimg.com/profile_images/1587405892437221376/h167Jlb2_400x400.jpg";
 
+    const dispatch = useDispatch();
     const posts = useSelector((state) => state.posts.posts);
     const loading = useSelector((state) => state.posts.loading);
+    const { currentUser } = useContext(AuthContext);
 
 
-    // useEffect(() => {
-    //     const token = localStorage.getItem("authToken");
-    //     if (token) {
-    //         const decodedToken = jwtDecode(token);
-    //         const userId = decodedToken.id;
-    //         dispatch(fetchPostsByUser(userId));
-    //     }
-    // }, [dispatch]);
+    useEffect(() => {
+        dispatch(fetchPostsByUser(currentUser.uid));
+    }, [dispatch, currentUser]);
 
     return (
         <Col sm={6} className="bg-light" style={{ border: "1px solid lightgrey" }}>
@@ -85,8 +82,7 @@ export default function ProfileMidBody() {
             {posts.map((post) => (
                 <ProfilePostCard
                     key={post.id}
-                    content={post.content}
-                    postId={post.id}
+                    post={post}
                 />
             ))}
         </Col>
--- src/components/ProfilePostCard.jsx
+++ src/components/ProfilePostCard.jsx
@@ -1,54 +1,32 @@
-import axios from "axios";
-import { jwtDecode } from "jwt-decode";
-import { useEffect, useState } from "react";
-import { Button, Col, Image, Row } from 'react-bootstrap'
+import { useContext, useState } from "react";
+import { Button, Col, Image, Row } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { likePost, removeLikeFromPost } from "../features/posts/postsSlice";
+import { AuthContext } from "./AuthProvider";
 
-export default function ProfilePostCard({ content, postId }) {
-    const [likes, setLikes] = useState([]);
+export default function ProfilePostCard({ post }) {
+    const { content, id: postId } = post;
+    const [likes, setLikes] = useState(post.likes || []);
+    const dispatch = useDispatch();
+    const { currentUser } = useContext(AuthContext);
+    const userId = currentUser.uid;
+    const isLiked = likes.includes(userId);
 
-    const token = localStorage.getItem("authToken");
-    const decode = jwtDecode(token)
-    const userId = decode.id;
 
 
     const pic = "https://pbs.twimg.com/profile_images/1587405892437221376/h167Jlb2_400x400.jpg";
-    const BASE_URL = 'https://009d1d9a-89c0-4823-8e6f-3a834072fc76-00-13687tvavm746.sisko.replit.dev'
-
-
-
-    useEffect(() => {
-        fetch(`${BASE_URL}/likes/post/${postId}`)
-            .then((response) => response.json())
-            .then((data) => setLikes(data))
-            .catch((error) => console.error("Error:", error));
-    }, [postId]);
-
-    const isLiked = likes.some((like) => like.user_id === userId);
 
     const handleLike = () => (isLiked ? removeFromLikes() : addToLikes());
 
     const addToLikes = () => {
-        axios.post(`${BASE_URL}/likes`, {
-            user_id: userId,
-            post_id: postId,
-        })
-            .then((response) => {
-                setLikes([...likes, { ...response.data, likes_id: response.data.id }]);
-            })
-            .catch((error) => console.error("Error:", error))
+        setLikes([...likes, userId]);
+        dispatch(likePost({ userId, postId }));
     }
 
     const removeFromLikes = () => {
-        const like = likes.find((like) => like.user_id === userId);
-        if (like) {
-            axios
-                .put(`${BASE_URL}/likes/${userId}/${postId}`)
-                .then(() => {
-                    setLikes(likes.filter((likeItem) => likeItem.user_id !== userId));
-                })
-                .catch((error) => console.error("Error:", error));
-        }
-    }
+        setLikes(likes.filter((id) => id !== userId));
+        dispatch(removeLikeFromPost({ userId, postId }));
+    };
 
     return (
         <Row
